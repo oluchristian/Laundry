b58943c6460133bf0667533c3bb1c113a44226bf
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-16 13:42:18 +0100

    added a view for the contact us page

---
+++ resources/views/home/call.blade.php
@@ -0,0 +1,17 @@
+<section class="container">
+    <section class="wantToWork-area" data-background="home/assets/img/gallery/section_bg01.png">
+        <div class="wants-wrapper w-padding2">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-xl-8 col-lg-9 col-md-7">
+                    <div class="wantToWork-caption wantToWork-caption2">
+                        <h2>Call us for a service</h2>
+                        <p>We deliver the goods to the most complicated places on earth</p>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-5">
+                    <a href="#" class="btn wantToWork-btn"><img src="home/assets/img/icon/call2.png" alt=""> Learn More</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</section>
