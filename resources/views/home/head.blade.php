d7952f37e733a82163f7c4c8f168d88b6adbf68b
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-16 13:56:59 +0100

    added a view for the header

---
+++ resources/views/home/head.blade.php
@@ -0,0 +1,34 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Laundry | Teamplate</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('home/assets/img/favicon.ico') }}">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{ asset('home/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/slicknav.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/progressbar_barfiller.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/gijgo.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/animated-headline.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/assets/css/style.css') }}">
+
+    <style>
+        .cart-btn-m
+        {
+            width: 150px; 
+            margin:auto;
+        }
+        
+    </style>
+</head>
