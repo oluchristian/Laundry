749d6d823756fd1b845e6c5596d5961df62ee84e
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-16 14:03:01 +0100

    added a view for slider

---
+++ resources/views/home/slider.blade.php
@@ -0,0 +1,18 @@
+<section class="slider-area hero-overly">
+    <div class="slider-active">
+        <!-- Single Slider -->
+        <div class="single-slider slider-height d-flex align-items-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-7 col-lg-9 col-md-10 col-sm-9">
+                        <div class="hero__caption">
+                            <h1 data-animation="fadeInLeft" data-delay="0.2s">Quality laundry service in your city</h1>
+                            <p data-animation="fadeInLeft" data-delay="0.4s">We take care about cleenness of your cloth</p>
+                            <a href="{{ route('user.view.services') }}" class="btn hero-btn" data-animation="fadeInLeft" data-delay="0.7s">Explore Services</a>
+                        </div>
+                    </div>
+                </div>
+            </div>          
+        </div>
+    </div>
+</section>
