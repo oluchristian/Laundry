679ac50d880e67780bb7b3b53daa1ccb3653aa4d
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-16 13:46:45 +0100

    added a view for the footer page

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,73 @@
+<footer>
+    <!-- Footer Start-->
+    <div class="footer-area footer-padding">
+        <div class="container">
+            <div class="row d-flex justify-content-between">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6">
+                 <div class="single-footer-caption mb-50">
+                   <div class="single-footer-caption mb-30">
+                    <!-- logo -->
+                    <div class="footer-logo mb-35">
+                        <a href="index.html"><img src="home/assets/img/logo/logo2_footer.png" alt=""></a>
+                    </div>
+                    <div class="footer-tittle">
+                        <div class="footer-pera">
+                            <p>Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
+                        </div>
+                    </div>
+                    <!-- social -->
+                    <div class="footer-social">
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="https://bit.ly/sai4ull"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
+            <div class="single-footer-caption mb-50">
+                <div class="footer-tittle">
+                    <h4>Services </h4>
+                    <ul>
+                        <li><a href="#">- Dry Cleaning</a></li>
+                        <li><a href="#">- Dry Clean</a></li>
+                        <li><a href="#">- Ironing Services</a></li>
+                        <li><a href="#">- Laundry Service London</a></li>
+                        <li><a href="#">- Laundry App</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
+            <div class="single-footer-caption mb-50">
+                <div class="footer-tittle">
+                    <h4>Get in touch</h4>
+                    <ul>
+                        <li class="number"><a href="#">(90) 898 789-8957</a></li>
+                        <li><a href="#">laundry@567.com</a></li>
+                        <li><a href="#">789/A, Green road NYC-9089</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- footer-bottom area -->
+<div class="footer-bottom-area section-bg2" data-background="home/assets/img/gallery/footer-bg.png">
+<div class="container">
+    <div class="footer-border">
+       <div class="row d-flex align-items-center">
+           <div class="col-xl-12 ">
+               <div class="footer-copy-right text-center">
+                   <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                      Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                      <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+<!-- Footer End-->
+</footer>
