444959b26881d503a2549944e4d64b307683c3a1
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-16 14:02:28 +0100

    added a view for services

---
+++ resources/views/home/services.blade.php
@@ -0,0 +1,47 @@
+<section class="services-area pt-top border-bottom pb-20 mb-60">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-7 col-lg-8">
+                <div class="section-tittle text-center mb-55">
+                    <span class="element">Our Process</span>
+                    <h2>This is how we work</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center">
+                    <div class="cat-icon">
+                        <img src="home/assets/img/icon/services-icon1.svg" alt="">
+                    </div>
+                    <div class="cat-cap">
+                        <h5><a href="services.html">We collect your clothes</a></h5>
+                        <p>The automated process starts as soon as your clothes go into the machine. The outcome is gleaming clothes!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center">
+                    <div class="cat-icon">
+                        <img src="home/assets/img/icon/services-icon2.svg" alt="">
+                    </div>
+                    <div class="cat-cap">
+                        <h5><a href="services.html">Wash your clothes</a></h5>
+                        <p>The automated process starts as soon as your clothes go into the machine. The outcome is gleaming clothes!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center">
+                    <div class="cat-icon">
+                        <img src="home/assets/img/icon/services-icon3.svg" alt="">
+                    </div>
+                    <div class="cat-cap">
+                        <h5><a href="services.html">Get delivery</a></h5>
+                        <p>The automated process starts as soon as your clothes go into the machine. The outcome is gleaming clothes!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
