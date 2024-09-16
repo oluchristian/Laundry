d183a260bee7090301396faccc668813d94e87df
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-16 17:04:37 +0100

    fixed some of the  views

---
+++ resources/views/home/testimonials.blade.php
@@ -0,0 +1,63 @@
+<section class="testimonials-area testimonials-overly  position-relative">
+    <div class="container">
+        <div class="border-bottom section-padding40 ">
+            <div class="row">
+                <div class="col-xl-12 ">
+                    <!-- testmonial-image -->
+                    <div class="testmonial-nav text-center">
+                        <div class="testmonial-thumb">
+                            <img src="home/assets/img/gallery/testimonila1.png" alt="">
+                        </div>
+                        <div class="testmonial-thumb">
+                            <img src="home/assets/img/gallery/testimonila2.png" alt="">
+                        </div>
+                        <div class="testmonial-thumb">
+                            <img src="home/assets/img/gallery/testimonila3.png" alt="">
+                        </div>
+                        <div class="testmonial-thumb">
+                            <img src="home/assets/img/gallery/testimonila2.png" alt="">
+                        </div>
+                    </div>
+                    <div class="testmonial-item-active text-center">
+                        <!-- testimonial-single-items -->
+                        <div class="testmonial-item ">
+                            <p class="pera">The automated process starts as soon as your clothes go into the<br> machine. The outcome is gleaming clothes!</p>
+                            <div class="rating">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <p> - Rupaya</p>
+                        </div>
+                        <!-- testimonial-single-items -->
+                        <div class="testmonial-item ">
+                            <p class="pera">The automated process starts as soon as your clothes go into the<br> machine. The outcome is gleaming clothes!</p>
+                            <div class="rating">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <p> - Rupaya</p>
+                        </div>
+                        <!-- testimonial-single-items -->
+                        <div class="testmonial-item ">
+                            <p class="pera">The automated process starts as soon as your clothes go into the<br> machine. The outcome is gleaming clothes!</p>
+                            <div class="rating">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <p> - Rupaya</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
