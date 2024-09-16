623f62c5231ad5a6ebf1aea5cd74304f126b0188
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-16 13:41:50 +0100

    added a view for the achievement page

---
+++ resources/views/home/achievement.blade.php
@@ -0,0 +1,45 @@
+<section class="services-area section-padding40 fix">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-7 col-lg-8">
+                <div class="section-tittle text-center mb-55">
+                    <span class="element">Fun Fact</span>
+                    <h2>Company achievement</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center">
+                    <div class="cat-cap">
+                        <span>4000</span>
+                        <p>The automated process starts as soon as your clothes go into the machine.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center">
+                    <div class="cat-cap">
+                        <span>300+</span>
+                        <p>The automated process starts as soon as your clothes go into the machine.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center">
+                    <div class="cat-cap">
+                        <span>95%</span>
+                        <p>The automated process starts as soon as your clothes go into the machine.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="bottom-bt">
+                        <img src="home/assets/img/gallery/company-bg.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
